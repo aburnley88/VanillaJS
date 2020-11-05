89596589993c5e8993f02e23987c1edbc6e05b24
aBurnley <aburnleylvx@outlook.com>
2020-11-05 09:31:14 -0500

    fixed typos finished proj

--- FormValidator/Script.js
+++ FormValidator/Script.js
@@ -19,13 +19,16 @@ function showSuccess (input) {
 
 //what is wrapped in the forward slashes is the regular expression of an email. object name is re
 //test method is called on re to determine if it is a string and then it is converted to lowercase
-function isValidEmail(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-    
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
 }
 
-//show success outline
+
 function checkRequired(inputArr) {
     let isRequired = false;
     inputArr.forEach(function(input) {
@@ -40,6 +43,25 @@ function checkRequired(inputArr) {
     return isRequired;
   }
 
+  //check input length
+  function checkLength(input, min, max) {
+    if(input.value.length < min)
+    {
+      showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+    } else if(input.value.length > max) {
+      showError(input, `${getFieldName(input)} must be less than ${max} characheters`)
+    } else {
+      showSuccess(input);
+    }    
+  }
+
+  //check that password entries match
+  function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+      showError(input2, 'Passowrds do not match');
+    }
+  }
+
   // Get fieldname
   //this method takes the input id and returns it with the first letter capitalized
 function getFieldName(input) {
@@ -50,5 +72,9 @@ function getFieldName(input) {
 form.addEventListener('submit', function(e) {
     e.preventDefault();
    
-    checkRequired([username, email, password, password2]);  
+    checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);  
+    checkEmail(email);
+    checkPasswordsMatch(password, password2)
 });
