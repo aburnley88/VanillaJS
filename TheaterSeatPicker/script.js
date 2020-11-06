2711231dae77acfe6984a5a4df3c4cce9f0897e6
aBurnley <aburnleylvx@outlook.com>
2020-11-06 11:53:16 -0500

    adding proj

---
+++ TheaterSeatPicker/script.js
@@ -0,0 +1,38 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row.seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+let ticketPrice = +movieSelect.value;
+
+//update total
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const selectedSeatsCount = selectedSeats.length;
+    console.log(selectedSeatsCount);
+    
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+//event listener
+//movie select event
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+});
+
+//event listener
+//seat event click
+container.addEventListener('click', e => {
+    if(e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+    ) {  
+        e.target.classList.toggle('selected');
+        
+        updateSelectedCount();
+    }
+
+});
